420cce10465ca5813d3b5d9f89498485eae2e72c
shubhamsharma6701 <75562025+shubhamsharma6701@users.noreply.github.com>
2021-05-19 16:03:19 +0530

    Update Insertion.cpp

--- Insertion.cpp
+++ Insertion.cpp
@@ -1,19 +1,19 @@
-#include<iostream> 
-#include<deque>
+#include<iostream> // For accessing the standard input/output classes of C++.
+#include<deque> // Header file for accessing the standard C++ class of deque and the built-in functions associated with it. 
 using namespace std;
-void pushindeque(int arr[], int n){
-    deque <int> q;
-    for(int i=0;i<n;i++)
+void pushindeque(int arr[], int n){ //Function definition for pushing the elements in the deque.
+    deque <int> q;  // Declaration of object 'q' of class 'deque'.
+    for(int i=0;i<n;i++)    // Pushing elements in the deque.
         q.push_back(arr[i]);
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)    //Printing the deque.
         cout<<q[i]<<" ";
 }
-int main(){
+int main(){ //Main function to input the number of elements and the elements to store them in an array. 
     int n;
-    cin>>n;
+    cin>>n; //
     int arr[n];
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)    //Storing the elements in an array. 
         cin>>arr[i];
-    pushindeque(arr,n);
+    pushindeque(arr,n); //Function call 
    return 0;
 }
